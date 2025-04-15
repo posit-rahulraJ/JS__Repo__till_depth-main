209ae82a4e193007a67d098755d34863ec6532e9
posit-rahulraJ <workrahulraj9@gmail.com>
2025-04-16 00:16:34 +0530

    "My second commit "

---
+++ 01_basics/let_const_var.js
@@ -0,0 +1,12 @@
+// a 
+/* comst cant be changed
+also we use mostly let and var whereas const use is generally avoided
+Keyword	    Scope	    Hoisting	        Reassignment	Redeclaration
+var	    Function-scoped	Yes	                    Yes	            Yes
+let	    Block-scoped	Yes (but uninitialized)	Yes	            No
+const	Block-scoped	Yes (but uninitialized)	No	            No
+
+ */
+const accountId =144553;
+// accountId =2
+console.log(accountId);
