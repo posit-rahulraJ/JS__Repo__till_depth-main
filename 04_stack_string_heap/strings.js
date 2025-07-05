d4d872195930bdfa10c25166b34a917c62ceaa64
posit-rahulraJ <workrahulraj9@gmail.com>
2025-07-05 15:08:05 +0530

    REVISION FIRST TIME

---
+++ 04_stack_string_heap/strings.js
@@ -0,0 +1,14 @@
+//string kya hai
+const name ="Rahul"
+const repo =50
+console.log(name + repo + "Value");
+
+//ham backticks `` use karenege kyunki ye modern approach hai kyunki ham string interpolationa auyr placeholders ka istemaal karte h
+console.log(`My name is ${name}  and repo count is  ${repo} `);
+console.log();
+
+const gameName = new String("Rahul Raj");
+console.log(gameName);
+
+
+
